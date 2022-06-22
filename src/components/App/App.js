e677fc01bea10930d15dc24b3f38041f0bd51a77
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-06-22 12:21:16 +0300

    Moved business information to App, Passed information from parent components (App) to child components (BusinessList, Business)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
 
+let business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+let businesses = [business, business, business,
+                  business, business, business]
+
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
